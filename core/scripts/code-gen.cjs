14537c97b5d71fe3f366a7f5d17726c3a5a71ccd
Yuxuan Zhang <zyx@z-yx.cc>
2026-01-29 03:04:51 -0500

    update core: auto generate index.cjs glue code for dist modules

--- core/scripts/code-gen.cjs
+++ core/scripts/code-gen.cjs
@@ -2,7 +2,7 @@ const { resolve } = require("path");
 const { writeFileSync, mkdirSync } = require("fs");
 const core = require("core");
 
-function gen(k, m) {
+function mjs(k, m) {
     const named_exports =
         typeof m === "object" ? Object.keys(m).map((f) => `    ${f},`) : [];
     return [
@@ -20,9 +20,20 @@ function gen(k, m) {
     ].join("\n");
 }
 
+function cjs(k, m) {
+    return [
+        "// ------------------------------------------------------",
+        "// Copyright (c) 2025 Yuxuan Zhang, zhangyuxuan@ufl.edu",
+        "// This source code is licensed under the MIT license.",
+        "// You may find the full license in project root directory.",
+        "// -------------------------------------------------------",
+        `module.exports = require("../index.cjs").${k};`,
+    ].join("\n");
+}
+
 for (const [k, m] of Object.entries(core)) {
     const dir = resolve(__dirname, "..", "dist", k);
     mkdirSync(dir, { recursive: true });
-    const file = resolve(dir, "index.mjs");
-    writeFileSync(file, gen(k, m), "utf-8");
+    writeFileSync(resolve(dir, "index.cjs"), cjs(k, m), "utf-8");
+    writeFileSync(resolve(dir, "index.mjs"), mjs(k, m), "utf-8");
 }
